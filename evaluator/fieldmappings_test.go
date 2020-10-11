f07dede04f815c902c653e679f6f65ee338e54e9
Bradley Kemp <bradleyjkemp@users.noreply.github.com>
2020-10-11 15:20:50 +0100

    Add (out-of-spec) support for JSONPath in fieldmappings (#1)
    * Add basic support for (non-standard) JSONPath field mappings
    
    * Add support for unmarshalling nested JSON

--- evaluator/fieldmappings_test.go
+++ evaluator/fieldmappings_test.go
@@ -44,3 +44,75 @@ func TestRuleEvaluator_HandlesBasicFieldMappings(t *testing.T) {
 		t.Error("If a field is mapped, the mapped name should work")
 	}
 }
+
+func TestRuleEvaluator_HandlesJSONPathFieldMappings(t *testing.T) {
+	rule := ForRule(sigma.Rule{
+		Logsource: sigma.Logsource{
+			Category: "category",
+			Product:  "product",
+			Service:  "service",
+		},
+		Detection: sigma.Detection{
+			Searches: map[string]sigma.Search{
+				"test": {
+					FieldMatchers: []sigma.FieldMatcher{{
+						Field:  "name",
+						Values: []string{"value"},
+					}},
+				},
+			},
+			Conditions: []sigma.Condition{
+				{Search: sigma.SearchIdentifier{Name: "test"}}},
+		},
+	}, WithConfig(sigma.Config{
+		FieldMappings: map[string]sigma.FieldMapping{
+			"name": {TargetNames: []string{"$.mapped.name"}},
+		},
+	}))
+
+	if rule.Matches(context.Background(), map[string]interface{}{
+		"name": "value",
+	}) {
+		t.Error("If a field is mapped, the old name shouldn't be used")
+	}
+
+	if !rule.Matches(context.Background(), map[string]interface{}{
+		"mapped": map[string]interface{}{
+			"name": "value",
+		},
+	}) {
+		t.Error("If a fieldmapping is a JSONPath expression, the nested field should be matched")
+	}
+}
+
+func TestRuleEvaluator_HandlesJSONPathByteSlice(t *testing.T) {
+	rule := ForRule(sigma.Rule{
+		Logsource: sigma.Logsource{
+			Category: "category",
+			Product:  "product",
+			Service:  "service",
+		},
+		Detection: sigma.Detection{
+			Searches: map[string]sigma.Search{
+				"test": {
+					FieldMatchers: []sigma.FieldMatcher{{
+						Field:  "name",
+						Values: []string{"value"},
+					}},
+				},
+			},
+			Conditions: []sigma.Condition{
+				{Search: sigma.SearchIdentifier{Name: "test"}}},
+		},
+	}, WithConfig(sigma.Config{
+		FieldMappings: map[string]sigma.FieldMapping{
+			"name": {TargetNames: []string{"$.mapped.name"}},
+		},
+	}))
+
+	if !rule.Matches(context.Background(), map[string]interface{}{
+		"mapped": `{"name": "value"}`,
+	}) {
+		t.Error("If a JSONPath expression encounters a string, the string should be parsed and then matched")
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bradleyjkemp/sigma-go
 go 1.15
 
 require (
+	github.com/PaesslerAG/jsonpath v0.1.1
 	github.com/alecthomas/participle v0.6.0
 	github.com/bradleyjkemp/cupaloy/v2 v2.6.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
