344f7807103699e1ca6cde9d7bbd825fab1d46e6
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2021-05-14 16:25:15 +0100

    Add InferFileType helper function

---
+++ rule_or_config.go
@@ -0,0 +1,35 @@
+package sigma
+
+import (
+	"gopkg.in/yaml.v3"
+)
+
+func InferFileType(contents []byte) FileType {
+	var fileType FileType
+	yaml.Unmarshal(contents, &fileType)
+	return fileType
+}
+
+type FileType string
+
+const (
+	UnknownFile FileType = ""
+	RuleFile    FileType = "rule"
+	ConfigFile  FileType = "config"
+)
+
+func (f *FileType) UnmarshalYAML(node *yaml.Node) error {
+	// Check if there's a key called "detection".
+	// This is a required field in a Sigma rule but doesn't exist in a config
+	for _, node := range node.Content {
+		if node.Kind == yaml.ScalarNode && node.Value == "detection" {
+			*f = RuleFile
+			return nil
+		}
+		if node.Kind == yaml.ScalarNode && node.Value == "logsources" {
+			*f = ConfigFile
+			return nil
+		}
+	}
+	return nil
+}
