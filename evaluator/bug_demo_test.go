1452b895f6263ad63388b39faa96f0eb50461a03
Liam Morris <43918149+lmoz25@users.noreply.github.com>
2022-12-21 15:16:34 +0000

    Add single config test case

--- evaluator/bug_demo_test.go
+++ evaluator/bug_demo_test.go
@@ -19,6 +19,30 @@ var logsourceBConfigYml []byte
 //go:embed test_rule.yml
 var testRuleYml []byte
 
+var event = map[string]interface{}{
+	"payload": map[string]interface{}{
+		"something": map[string]interface{}{
+			"user_id": "abc123",
+		},
+	},
+}
+
+// Case where we only load one config
+func TestOnePaths(t *testing.T) {
+	logsourceAConfig, err := sigma.ParseConfig(logsourceAConfigYml)
+	assert.NoError(t, err)
+	testRule, err := sigma.ParseRule(testRuleYml)
+	assert.NoError(t, err)
+
+	ruleEvaluator := ForRule(testRule, WithConfig(logsourceAConfig), WithPlaceholderExpander(placeholderExpander))
+
+	result, err := ruleEvaluator.Matches(context.Background(), event)
+	assert.NoError(t, err)
+	assert.True(t, result.SearchResults["TestCondition"])
+
+}
+
+// Case where we load two configs
 func TestMultipleSamePaths(t *testing.T) {
 	logsourceAConfig, err := sigma.ParseConfig(logsourceAConfigYml)
 	assert.NoError(t, err)
@@ -26,14 +50,6 @@ func TestMultipleSamePaths(t *testing.T) {
 	assert.NoError(t, err)
 	testRule, err := sigma.ParseRule(testRuleYml)
 	assert.NoError(t, err)
-
-	event := map[string]interface{}{
-		"payload": map[string]interface{}{
-			"something": map[string]interface{}{
-				"user_id": "abc123",
-			},
-		},
-	}
 
 	ruleEvaluator := ForRule(testRule, WithConfig(logsourceAConfig, logsourceBConfig), WithPlaceholderExpander(placeholderExpander))
 
